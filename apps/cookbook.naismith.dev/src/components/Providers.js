89591707c9a61979f7f61a23b55cf0e4cd4251e3
Chris Naismith <chris.naismith@grain.co>
2026-01-31 23:26:39 -0500

    remove old app

--- apps/cookbook.naismith.dev/src/components/Providers.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from '../common/theme';
-
-export const Providers = ({ children }) => (
-  <ThemeProvider theme={theme}><CssBaseline />{children}</ThemeProvider>
-);
