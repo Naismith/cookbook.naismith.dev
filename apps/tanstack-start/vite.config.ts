66ccdf083e130e9a767ddcc699d534817b340edd
Chris Naismith <chris.naismith@grain.co>
2026-06-14 16:58:13 -0400

    Update dependencies and configuration for tanstack-start app

--- apps/tanstack-start/vite.config.ts
+++ apps/tanstack-start/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
 
 import tailwindcss from '@tailwindcss/vite'
@@ -10,6 +9,7 @@ import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   resolve: {
+    tsconfigPaths: true,
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
@@ -17,10 +17,6 @@ const config = defineConfig({
   plugins: [
     devtools(),
     netlify(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
